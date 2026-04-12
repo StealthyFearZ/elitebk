916c81251094b3d4f63f237b2ac89f7dea1a19ce
matthewnichols8 <mattnichols192@icloud.com>
2026-04-12 11:15:34 -0400

    Added login and upload context view functions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from .views import ChatAnswerView
+from .views import ChatAnswerView, LoginView, UploadContextView
 
 urlpatterns = [
     path('ask/', ChatAnswerView.as_view(), name='api.ask'),
     path('update-dataset/', views.update_dataset_view, name='update_dataset'),
+    path('login/', LoginView.as_view(), name='api.login'), # Used for deciding dev user vs enduser
+    path('upload-context/', UploadContextView.as_view(), name='api.upload_context'), # Used for uplod dataset
 ]
