52af5b919421eb12da0ab6bbc3d595f9b538d53d
matthewnichols8 <mattnichols192@icloud.com>
2026-04-22 21:27:56 -0400

    added url for report

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import ChatAnswerView, LoginView, UploadContextView, RegisterView
+from .views import ChatAnswerView, LoginView, UploadContextView, RegisterView, GenerateReportView
 
 
 urlpatterns = [
     path('ask/', ChatAnswerView.as_view(), name='api.ask'),
+    path('generate-report/', GenerateReportView.as_view(), name='api.generate_report'), # Generates the pdf reportg
     path('update-dataset/', views.update_dataset_view, name='update_dataset'),
     path('login/', LoginView.as_view(), name='api.login'), # Used for deciding dev user vs enduser
     path('upload-context/', UploadContextView.as_view(), name='api.upload_context'), # Used for uplod dataset
