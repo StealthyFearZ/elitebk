cb53894fd09b6b51a4084a49829c51b16e4ae950
StealthyFearZ <daksh.raghuvanshi.24@seoulforeign.org>
2026-04-27 03:56:06 -0400

    Registers ChatMessage and generates an admin that tinkers with the exact way the values willbe used and interacted with.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ChatTelemetry
+from .models import ChatTelemetry, ChatMessage
 
 @admin.register(ChatTelemetry)
 class ChatTelemetryAdmin(admin.ModelAdmin):
@@ -15,3 +15,15 @@ class ChatTelemetryAdmin(admin.ModelAdmin):
         if obj.latency_ms >= 1000:
             return f"{obj.latency_ms / 1000:.3f} s"
         return f"{obj.latency_ms:.0f} ms"
+    
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    # Same format used from ChatTelemetryAdmin
+    list_display = ('id', 'short_query', 'intent', 'response_time_seconds', 'created_at') # how all fields should be displayed
+    list_filter = ('intent', 'created_at') # filters for the displaying of each of the objects
+    search_fields = ('query', 'intent', 'ai_response') # what should be searchable from a list of recorded ChatMessage objects
+    readonly_fields = ('session_id', 'user_query', 'ai_response', 'intent', 'response_time', 'created_at') # fields that should only have values that can be read, not edited
+
+    ordering = ['-created_at'] # order the list of items by descending order of object creation, most recent to oldest
+
+
